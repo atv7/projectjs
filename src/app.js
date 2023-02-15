58357968c4714f12d429f874cd472f5ca10fe16e
atv <example@gmail.com>
2023-02-15 16:36:57 +0400

    added index.html

--- src/app.js
+++ src/app.js
@@ -1,4 +1,18 @@
-'use strict';
+"use strict";
 
-console.log('testik');
-// 
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const resultFilm = prompt("Один из последних просмотренных фильмов?", "");
+const resultRating = prompt("На сколько его оцените?", "");
+
+personalMovieDB.movies[resultFilm] = resultRating;
+
+console.log(personalMovieDB)
