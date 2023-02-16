14439eb9f0549d78e1f094b3c6b1158f78d8c37c
atv <example@gmail.com>
2023-02-17 00:46:41 +0400

    update

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
-console.log('123');
-// 
+"use strict"
+
+for (let i = 0; i < 10; i++) {
+
+    // если true, пропустить оставшуюся часть тела цикла
+    if (i % 2 == 0) continue;
+
+        console.log(i); // 1, затем 3, 5, 7, 9
+  }
