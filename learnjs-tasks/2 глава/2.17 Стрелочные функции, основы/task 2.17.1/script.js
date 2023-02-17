39124f91fa766062d8eedb0c71c6c19f937c4de1
Artem <ateter17@gmail.com>
2023-02-17 21:04:58 +0400

    update

--- learnjs-tasks/2 глава/2.17 Стрелочные функции, основы/task 2.17.1/script.js
+++ learnjs-tasks/2 глава/2.17 Стрелочные функции, основы/task 2.17.1/script.js
@@ -17,12 +17,13 @@ ask(
 
 
 function ask(question, yes, no) {
-    if (confirm(question)) yes()
-    else no();
+  if (confirm(question)) yes()
+  else no();
 }
 
 ask(
-    "Вы согласны?",
-    () => alert("Вы согласились."),
-    () => alert("Вы отменили выполнение.")
+  "Вы согласны?",
+  () => alert("Вы согласились."),
+  () => alert("Вы отменили выполнение.")
 );
+
